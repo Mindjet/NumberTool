1f777f1d118b6033a201e202aa94fdc8dfbef2f1
Mindjet <578746875@qq.com>
2016-09-09 18:03:13 +0800

    finish UI of MainActivity

--- app/src/main/java/mindjet/com/numbertool/MainActivity.java
+++ app/src/main/java/mindjet/com/numbertool/MainActivity.java
@@ -1,7 +1,10 @@
 package mindjet.com.numbertool;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //immersiveMode();
+
+    }
+
+    private void immersiveMode() {
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+
+            Window window = getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        }
     }
 }
